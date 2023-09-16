205cc29863ad962a97d14f95eed68970d1825ce7
Henry Cheung <cheetah_henry@hotmail.com>
2023-09-16 10:21:30 +0800

    change code due to idf's change in API

--- src/main.c
+++ src/main.c
@@ -4,7 +4,8 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
-#include "esp_spi_flash.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
 #include "led_strip.h"
 
 #define RGB_PIN 8
@@ -27,14 +28,19 @@ void app_main(void) {
     esp_chip_info_t chip_info;
     esp_chip_info(&chip_info);
 
-    printf("\n%s with %d CPU core(s), Revision: %d, WiFi%s%s, ",
+    printf("\n%s with %d CPU core(s), Revision: %d, WiFi%s%s%s, ",
             CONFIG_IDF_TARGET,
             chip_info.cores,
             chip_info.revision,
             (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-    printf("%dMB %s flash\n", spi_flash_get_chip_size() / (1024 * 1024),
-            (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+            (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "",
+            (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : "");
+    uint32_t size_flash_chip;
+    esp_flash_get_size(NULL, &size_flash_chip);
+    printf("%luMB %s flash\n", size_flash_chip / (1024 * 1024), 
+          (chip_info.features & CHIP_FEATURE_EMB_FLASH) ? "embedded" : "external");
+    
+     printf("Minimum free heap size: %lu bytes\n", esp_get_minimum_free_heap_size());
 
     rgb = led_strip_init(0, RGB_PIN, 1);    // configure RGB
     rgb->clear(rgb, 50);                     // clear all LEDs
